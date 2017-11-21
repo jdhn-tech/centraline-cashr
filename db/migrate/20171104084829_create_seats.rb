cb239819b9ae118c882515830ee30c30bce64105
Duy Hai Nguyen <dnguyen.etudes@gmail.com>
2017-11-21 09:27:01 +0100

    model tweaking

--- db/migrate/20171104084829_create_seats.rb
+++
@@ -1,9 +0,0 @@
-class CreateSeats < ActiveRecord::Migration[5.1]
-  def change
-    create_table :seats do |t|
-      t.references :table
-      t.boolean :occupied
-      t.timestamps
-    end
-  end
-end
